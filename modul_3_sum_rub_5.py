d157a0b99d70b25506716ae74f5af9f4eab4f9f7
Natali0125 <seminatalya2013@gmail.com>
2024-12-13 18:42:36 +0300

    Update modul_3_sum_rub_5.py

--- modul_3_sum_rub_5.py
+++ modul_3_sum_rub_5.py
@@ -70,33 +70,17 @@ def get_multiplied_digits(number):
         return first * get_multiplied_digits(int(str_number[1:]))
 
 print(get_multiplied_digits(40203))
-#
-#
-#
-#
-#
-#
-#         # result = get_multiplied_digits(40203)
-#         # print(result)
-#
-# result2 = get_multiplied_digits(402030)
-# print(result2)
-#
-# result_3 = get_multiplied_digits(870887323000)
-# print(result_3)
-#         #
-#
+
+
+result2 = get_multiplied_digits(402030)
+print(result2)
+
+result_3 = get_multiplied_digits(870887323000)
+print(result_3)
+        
 
 
 
 
-#
-#     # if str_number[-1] == 0:
-#     #     first = str_number[-1] = 1
-#     #     return first
-#
-#
-# #
 
-#
 
